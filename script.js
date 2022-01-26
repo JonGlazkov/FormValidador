dd50dffd2c0001ec9a383977d743db7e60ed1860
Jonathan <jonathan_vmacedo@hotmail.com>
2022-01-26 17:18:40 -0300

    Formulário com Validação

---
+++ script.js
@@ -0,0 +1,90 @@
+let validadorForm = {
+    handleSubmit: (event) => {
+        event.preventDefault();
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        validadorForm.clearErrors();
+
+        for (let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+            let check = validadorForm.checkInput(input);
+            if (check !== true) {
+                send = false;
+                validadorForm.showError(input, check);
+            }
+        }
+
+        if (send) {
+            form.submit();
+        }
+    },
+    checkInput: (input) => {
+        let rules = input.getAttribute('data-rules');
+        if (rules !== null) {
+            rules = rules.split('|');
+            for (let k in rules) {
+                let rDetails = rules[k].split('=');
+                switch (rDetails[0]) {
+                    case 'required':
+                        if (input.value == '') {
+                            return 'Campo deve ser preenchido.';
+                        }
+
+                        break;
+                    case 'min':
+                        if (input.value.length < rDetails[1]) {
+                            return 'Campo tem que ter pelo menos ' + rDetails[1] + ' caracteres.';
+
+                        }
+                        break;
+                    case 'email':
+                        if(input.value) {
+                            let regex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            if (!regex.test(input.value.toLowerCase())) {
+                                send = false;
+                                return 'E-mail digitado não é válido';
+                            }
+                        }
+                        break;
+                    case 'confirmed':
+                        let confirmation = form.querySelector('input[name=senha_confirmacao]')
+                        if (! confirmation) {
+                            continue
+                        }
+                        if (input.value != confirmation.value) {
+                            send = false;
+                            validadorForm.showError(confirmation, 'Senhas não conferem.')
+                            return 'Senhas não conferem.'
+                        }
+                        break;
+                }
+            }
+        }
+        return true;
+    },
+    showError: (input, error) => {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors: () => {
+        let inputs = form.querySelectorAll('input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for (let i = 0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
+    }
+}
+
+let form = document.querySelector('.formulario');
+form.addEventListener('submit', validadorForm.handleSubmit);
